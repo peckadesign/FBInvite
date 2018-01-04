aa27e1d904cfcbe420bb1e0d133154f94ae531c1
Premysl Posker <premysl.posker@peckadesign.cz>
2018-01-04 17:37:12 +0100

    viz readme

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # FBInvite
 This plugin allows you to invite all people who liked your post from Facebook dialog with simple click.
 
-# How to use
+## How to use
 1. Click on link with people who liked your post
 2. Let the dialog appear 
 3. Use `+ Invite users` button in the extension
@@ -11,9 +11,19 @@ This plugin allows you to invite all people who liked your post from Facebook di
 
 ![Screenshot](doc/step-2.jpg)
 
-# How to install
+## How to install
 1. Download `download/FBInvite.zip`, unpack and save it to your disk
 2. Open Chrome browser with new tab `chrome://extensions` or `Settings > Extensions`
 3. Check the `Developer mode` in upper right corner
 4. Click `Load unpacked extension...` and locate the directory you just saved
 5. FBInvite extension should appear in your extension list
+
+## Changelog
+
+### 1.1
+- Fixed broken Classnames/IDs (due to FB changed HTML markup)
+- Plugin automatically go through the dialog and invite all the users (even if more users needs to be loaded)
+- User is informed, when script finishes and how many users had been invited
+
+### 1.0
+- Plugin release
--- source/popup.js
+++ source/popup.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     var inviteButton = document.getElementById('fbInviteBtn');
+
     inviteButton.addEventListener('click', function() {
-
-        chrome.tabs.executeScript(null,{code:"var allElements = document.getElementsByTagName('a');for (var i = 0; i < allElements.length; i++) {var attr = allElements[i].getAttribute('ajaxify');if (attr && attr.startsWith('/ajax/pages/invite/send_single/?')) {allElements[i].click();}}"});
+        chrome.tabs.executeScript(null,{code : "var invitedCount=0;var information=document.createElement('div');var sp2=document.querySelector('ul[id^=\"reaction_profile_browser\"]');var parentDiv=document.querySelector('ul[id^=\"reaction_profile_browser\"]').parentNode;information.style.background='#e2db1a';information.style.padding='5px 10px';information.style.margin='5px 0';information.style.color='#000';information.style.borderRadius='3px';information.innerHTML='Inviting users ...';parentDiv.insertBefore(information,sp2);var invite=function(){var inviteButtons=document.querySelector('ul[id^=\"reaction_profile_browser\"]').querySelectorAll('a[role=button][ajaxify]');var loadButton;var end=false;if(document.getElementById('reaction_profile_pager')){loadButton=document.getElementById('reaction_profile_pager').getElementsByTagName('a');if(loadButton.length){loadButton[0].click();setTimeout(function(){invite()},2000)}else{end=true}}else{end=true}for(var i=0;i<inviteButtons.length;i++){inviteButtons[i].click();invitedCount++}if(end){information.style.background='#2f9207';information.style.color='#fff';information.innerHTML=invitedCount+' users successfully invited :)'}};invite();"});
         window.close();
-
     }, false);
 }, false);
 
